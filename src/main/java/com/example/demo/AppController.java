cca13526ac80bf60e2cb03bcad1b16f9bcecd230
hello <hello@DESKTOP-G5MNAT4>
2021-05-18 13:27:01 +0530

    new

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/App")
+@RestController
+public class AppController {
+
+	
+	
+	@GetMapping("/getApp")
+	ResponseEntity m1() {
+		
+		return new ResponseEntity("Application Is Running", null, 200);
+		
+	}
+}
---
+++ src/main/java/com/example/demo/DietServices1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietServices1Application.class, args);
+	}
+
+}
